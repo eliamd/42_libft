2a93db81a8c248e40689b35e19b223c83c4b7543
Eliam <40575511+eliamd@users.noreply.github.com>
2024-10-20 16:54:14 +0200

    # MOD : Add free on main

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 10:31:59 by edetoh            #+#    #+#             */
-/*   Updated: 2024/10/20 15:49:58 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/10/20 16:53:02 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,18 +107,20 @@ char	**ft_split(char const *str, char del)
 
 // int	main(void)
 // {
-// 	char *str = "";
+// 	char *str = "bonjourzbonjourzbuhuhuzgyguhzohh";
 // 	char del = 'z';
 // 	int i = 0;
 
 // 	char **res = ft_split(str, del);
 
 // 	printf("===== RESULT =====\n");
-// 	while (i < 20)
+// 	while (res[i] != NULL)
 // 	{
 // 		printf("Tab DE %i : %s\n", i, res[i]);
+// 		free(res[i]);
 // 		i++;
 // 	}
 // 	printf("==================\n");
+// 	free(res);
 // 	return (1);
 // }
