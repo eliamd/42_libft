7c1cf8b7c68e479db070d42e48bcd66afb1649f3
Eliam <Texsword76@gmail.com>
2024-10-21 13:27:54 +0200

    + ADD : lstsize (bonus)

---
+++ ft_lstsize_bonus.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 13:21:33 by edetoh            #+#    #+#             */
+/*   Updated: 2024/10/21 13:27:35 by edetoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	result;
+
+	result = 1;
+	while (lst->next != NULL)
+	{
+		*lst = *lst->next;
+		result++;
+	}
+	return (result);
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "libft.h"
+
+// int main(void)
+// {
+// 	t_list *node;
+// 	char *content = "Hello, world!";
+
+// 	node = ft_lstnew(content);
+// 	if (node)
+// 	{
+// 		printf("Node content: %s\n", (char *)node->content);
+// 		printf("Node next: %p\n", (void *)node->next);
+
+// 		t_list *newbuble1;
+// 		char *newbuble1_content = "Coucou";
+
+// 		// Allouer de la mémoire pour newbuble1
+// 		newbuble1 = ft_lstnew(newbuble1_content);
+// 		if (newbuble1)
+// 		{
+// 			ft_lstadd_front(&node, newbuble1);
+// 			printf("Node content after add: %s\n", (char *)node->content);
+// 			printf("Node next after add: %p\n", (void *)node->next);
+// 			printf(">>>>> Taille : %i", ft_lstsize(newbuble1));
+// 		}
+// 		else
+// 		{
+// 			printf("Failed to create newbuble1\n");
+// 		}
+// 	}
+// 	else
+// 	{
+// 		printf("Failed to create node\n");
+// 	}
+
+// 	// Libérer la mémoire allouée
+// 	free(node);
+// 	return 0;
+// }
+
